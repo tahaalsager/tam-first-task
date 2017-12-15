88a13195257461ac38b5a86ea30745b9692e226c
Taha Alsager <tahaalsager@MacBook-Pro-alkhas-b-Taha.local>
2017-12-15 19:10:46 +0300

    final version

---
+++ js/tam.js
@@ -0,0 +1,65 @@
+//first card
+        function myFunction() {
+            document.getElementById("list-1").style.textDecoration = "line-through";
+            document.getElementById("list-1").style.color = "#a8a7a7";
+            document.getElementById('redu-1').src = 'img/icon-check.svg';
+
+        }
+
+        function myFunction2() {
+            document.getElementById("list-2").style.textDecoration = "line-through";
+            document.getElementById("list-2").style.color = "#a8a7a7";
+            document.getElementById('redu-2').src = 'img/icon-check.svg';
+        }
+
+        function myFunction3() {
+            document.getElementById("list-3").style.textDecoration = "line-through";
+            document.getElementById("list-3").style.color = "#a8a7a7";
+            document.getElementById('redu-3').src = 'img/icon-check.svg';
+        }
+
+        function myFunction4() {
+            document.getElementById("list-4").style.textDecoration = "line-through";
+            document.getElementById("list-4").style.color = "#a8a7a7";
+            document.getElementById('redu-4').src = 'img/icon-check.svg';
+        }
+
+        function myFunction5() {
+            document.getElementById("list-5").style.textDecoration = "line-through";
+            document.getElementById("list-5").style.color = "#a8a7a7";
+            document.getElementById('redu-5').src = 'img/icon-check.svg';
+        }
+//scond card
+function myFunction6() {
+            document.getElementById("list-6").style.textDecoration = "line-through";
+            document.getElementById("list-6").style.color = "#a8a7a7";
+            document.getElementById('redu-6').src = 'img/icon-check.svg';
+        }
+   
+    function myFunction7() {
+            document.getElementById("list-7").style.textDecoration = "line-through";
+            document.getElementById("list-7").style.color = "#a8a7a7";
+            document.getElementById('redu-7').src = 'img/icon-check.svg';
+        }
+function myFunction8() {
+            document.getElementById("list-8").style.textDecoration = "line-through";
+            document.getElementById("list-8").style.color = "#a8a7a7";
+            document.getElementById('redu-8').src = 'img/icon-check.svg';
+        }   
+function myFunction9() {
+            document.getElementById("list-9").style.textDecoration = "line-through";
+            document.getElementById("list-9").style.color = "#a8a7a7";
+            document.getElementById('redu-9').src = 'img/icon-check.svg';
+        }   
+
+//third card
+function myFunction10() {
+            document.getElementById("list-10").style.textDecoration = "line-through";
+            document.getElementById("list-10").style.color = "#a8a7a7";
+            document.getElementById('redu-10').src = 'img/icon-check.svg';
+        }   
+function myFunction11() {
+            document.getElementById("list-11").style.textDecoration = "line-through";
+            document.getElementById("list-11").style.color = "#a8a7a7";
+            document.getElementById('redu-11').src = 'img/icon-check.svg';
+        }   
